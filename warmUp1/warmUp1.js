15299303ebaac35fe10e79bab99d4f3183c2d4b8
= <=>
2020-02-01 09:31:16 +0100

    morning warmup

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,29 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+function comb(firtName, lastName){
+	return firtName + " " + lastName;
+}
+
+function divByThree(){
+	var a = 13, b = 3;
+	if(a%b === 0) return true
+	else return false;
+}
+
+function averageAge(){
+	var arr = [13,14,13,15,16,17,19,13,16,15];
+	var sum = 0;
+	for(var i=0; i<arr.length; ++i) 
+		sum += arr[i];
+	return sum/arr.length;
+
+}
+
+function ageInSec(age){
+	var today = 58786560000;
+	var dateOfBirth = 57886617600 + 2419200;
+	return today - dateOfBirth;
+}
