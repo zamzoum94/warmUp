ca3de9943159db3f2a1ec1e9ac10dd4feb204dfb
siwarhkimi <cywarhkimi@gmail.com>
2020-02-01 13:29:00 +0100

    SiwarWarmUp

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,53 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+//1
+function name(firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+//'Siwar'+' Hkimi';
+
+//2
+function multipleOrNot(x, y) {
+	if (x % y === 0) {
+		return x + " is a multiple of " + y + ".";
+	}
+	else {
+		return x + " is not a multiple of " + y + ".";
+	}
+}
+//13%3===1 different from 0 so 13 isn't a multiple of 3
+
+//3
+function average(arr) {
+	var i = 0;
+	var average = 0;
+	while (i < arr.length) {
+		average = average + arr[i]
+		i++
+	}
+	return average / arr.length
+}
+
+//average=(13+14+13+15+16+17+19+13+16+15)/10;
+
+//4
+// function ageInSeconds(day,month,year){
+// var ageYear=0;
+// var ageMonth=0;
+// var ageDay=0;
+// ageYear=(2020-year)*365*24*60;
+// var array=['January','February','March','April','May','June','July','August','Septembre','October','November','December']
+// if(month==='January'||month==='March'||month==='May'||month==='July'||month==='Septembre'||month==='Novembre'){
+// 	ageMonth=31*24*60;
+// }	
+// else if(month==='April'||month==='June'||month==='August'||month==='October'||month==='December'){
+// 	ageMonth=30*24*60;
+// }
+// else {
+// 	ageMonth=28*24*60;
+// }
+// 
+// }
+//age=26*365*24*60
