6484abe4f43eda4b43e0509cbfb2984217006032
siwarhkimi <cywarhkimi@gmail.com>
2020-02-04 09:56:50 +0100

    Warmup 3 Siwar

--- warmUp3/warmUp3.js
+++ warmUp3/warmUp3.js
@@ -2,7 +2,12 @@
 // - takes 2 arguments, both numbers.
 // - returns whichever number is the greater (higher) number.
 // ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
-
+function greaterNum(num1, num2) {
+    if (num1 >= num2) {
+        return "The greater number of " + num1 + " and " + num2 + " is " + num1 + ".";
+    }
+    return "The greater number of " + num1 + " and " + num2 + " is " + num2 + ".";;
+}
 
 
 // 2) Write a function named isEven using  for loop that 
@@ -10,18 +15,44 @@
 // -  return array contain the even values,
 //  ex:  isEven(1,10) => [2,4,6,8,10]
 
-
-//3) write a function named sum that 
+// function isEven(x, y) {
+//     var arr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         while (x <= y) {
+//             if (x % 2 === 0) {
+//                 arr[i] = arr.push(x)
+//             }
+//             x++
+//         }
+//     }
+//     return arr;
+// }
+//3) write a function named sum that
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
-
+function sum(x, y) {
+    var sum = 0;
+    while (x <= y) {
+        sum = sum + x;
+        x++;
+    }
+    return sum;
+}
 
 //4) Write a function named factorial that 
 // -  Use Recursion to calculate the factorial of a number
 // -  the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n
 // - 5! = 5*4*3*2*1 = 120
 // ex : factorial(5) => 120 
+function factorial(n) {
+    var fact = 1;
+    if (n === 0) {
+        return 1
+    }
+    fact = fact * n;
+    return fact * factorial(n - 1)
 
+}
 
 //5) write a function named decimals
 //- the function will format a number up to specified decimal places
@@ -34,3 +65,18 @@
 //      decimals(2.100212, 3) ==> '2.100'
 //      decimals(2100, 2) ==> '2100.00'
 //      
+function decimals(num1, num2) {
+    var ind = 0
+    var decim = ''
+    var str = ''
+    if ((typeof num1 !== 'number') || (typeof num2 !== 'number')) {
+        return false;
+    }
+    else {
+        ind = (num1 + '').indexOf('.');
+        decim = (num1 + '').slice(ind + 1)
+        str = (num1 + '').slice(0, ind + 1 + num2)
+        return str;
+
+    }
+}
