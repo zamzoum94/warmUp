324f201c02ca2d6c79684a8829696dd310627957
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-02-06 08:53:09 +0100

    warmUp5

---
+++ warmUp5/warmUp5.js
@@ -0,0 +1,9 @@
+// 1-Choose the correct comparison operator to display "true", when: 5 is less than 7.
+//2- Write a function named helloWorld that:
+//takes 1 argument, a language code (e.g. "fr", "es", "en")
+//returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
+//ex: helloWorld('fr') ==> 'Bonjour tout le monde'
+//    helloWorld('es') ==> 'Hola, Mundo'
+//    helloWorld('') ==> 'Hello, World'        
+//3- write a function to Reverse an array without use array.reverse() method:
+// ex: reverseArray([1,2,3]) ==> [3,2,1]
