8bf02ee242f3a8b7b9df413c0ca6162aaa54e039
siwarhkimi <cywarhkimi@gmail.com>
2020-02-15 10:08:46 +0100

    Siwar :)

--- warmUp11/warmUp11.js
+++ warmUp11/warmUp11.js
@@ -9,3 +9,74 @@
     • Write a function searchMates that, given a query and an array of Mates,
       searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 */
+
+function myClassMates(name, hair, gender, age, glasses) {
+  return {
+    name: name,
+    hair: hair,
+    gender: gender,
+    age: age,
+    glasses: glasses
+  };
+}
+
+var houssem1 = myClassMates("Houssem", "Black", "Male", "30", true);
+var houssem2 = myClassMates("Houssem", "Black", "Male", "19", false);
+var nouha = myClassMates("Nouha", "Black", "Female", "33", true);
+var taha = myClassMates("Taha", "Black", "Male", "40", true);
+var souha = myClassMates("Souha", "Red", "Female", "23", false);
+var classMates = [houssem1, houssem2, nouha, taha, souha];
+
+function displayFriend(mate) {
+  if (mate.gender === "Male") {
+    if (mate.glasses === true) {
+      return "My mate " + mate.name + " has a " + mate.hair + " hair " + " and he is " + mate.age + " years old.\n He also wears glasses.";
+    }
+    else {
+      return "My mate " + mate.name + " has a " + mate.hair + " hair " + " and he is " + mate.age + " years old.\n He doesn't wear glasses.";
+    }
+  }
+  else {
+    if (mate.glasses === true) {
+      return "My mate " + mate.name + " has a " + mate.hair + " hair " + " and she is " + mate.age + " years old.\n She also wears glasses.";
+    }
+    else {
+      return "My mate " + mate.name + " has a " + mate.hair + " hair " + " and she is " + mate.age + " years old.\n She doesn't wear glasses.";
+    }
+  }
+}
+
+function addFriend(mate) {
+  var arr = classMates.push(mate);
+
+  return arr;
+}
+
+function nbOfMale(array) {
+  var counter = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].gender === 'Male') {
+      counter = counter + 1;
+    }
+  }
+  return counter;
+}
+
+
+function searchMates(query, array) {
+  var arr = [];
+  var str = "";
+
+  for (var i = 0; i < array.length; i++) {
+    arr = Object.keys(array[i]);
+    for (var j = 0; j < arr.length; j++) {
+      if ((arr[j].query !== undefined) && (arr[j].query === query)) {
+        str = str + array[i]["name"] + "\n";
+      }
+    }
+  }
+  return str + "\n the people above match together.";
+}
+
+// I didn't verify the functions in the console because of the time so I think there exist plenty mistakes :'( especially the last one
