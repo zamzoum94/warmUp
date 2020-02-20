b8a430e8ea2c4d0e1e78f81ef8a28a628ba965c6
ghostrib <sh4rkbot@gmail.com>
2020-02-20 08:49:45 +0100

    warmup 14

---
+++ warmUp14/warmUp14.js
@@ -0,0 +1,10 @@
+// Create an HTML file
+// Create a CSS file
+// Link the CSS file to the HTML file
+// The page should have a header, body and footer
+// Change the background color of the body to be blue
+// Insert three images next to each other
+// Create two buttons and use a different font family for each button
+// Create an unordered list with three anchor tags in it
+// Place a copyright claimer in the footer
+// Have Fun
